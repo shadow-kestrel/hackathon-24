3cfe36d0cba114e777e5b46afd74125fdc8780e6
raccnin <johncw004@gmail.com>
2024-02-17 22:47:13 +0000

    consolidate Ethan's notebook into singular file

--- server/Python/app.py
+++ server/Python/app.py
@@ -1,85 +1,41 @@
-import nltk
-import spacy
+import utils
 
-from nltk.probability import FreqDist
-from nltk.tokenize import word_tokenize
-from nltk.tag import pos_tag
-
-def get_tf(term, document):
-
-    delimiter = " "
-    term_normalized = delimiter.join(utils.canonize(term))
-
-    # print(term_normalized)
-
-    numOfAppearancesInDoc = document.count(term_normalized)
-    totalNumOfTermsInDoc = len(document)
-
-    # print("NumOfAppearancesInDoc: {}".format(numOfAppearancesInDoc))
-    # print("TotalNumOfTermsInDoc: {}".format(totalNumOfTermsInDoc))
-  
-    if (totalNumOfTermsInDoc != 0):
-        return numOfAppearancesInDoc / totalNumOfTermsInDoc
-    else:
-        return 0
+'''
+with open("tweets.txt", "r") as tweets:
+    tweet_list = tweets.read().split("\n")
     
-def get_idf(term, corpus):
-
-    delimiter = " "
-    term_normalized = delimiter.join(utils.canonize(term))
-
-    numOfDocsInCorpus = len(corpus)
-    numOfAppearrancesInCorpus = 0
-    
-    for document in corpus:
+    for line in tweets.read().split("\n"):
         
-        if term_normalized in document:
-            
-            numOfAppearrancesInCorpus += 1
-
-    # print(numOfAppearrancesInCorpus)
-
-    if (numOfAppearrancesInCorpus != 0):
-        return numOfDocsInCorpus / numOfAppearrancesInCorpus
-    else:
-        # Return high IDF value as the term is very uncommon
-        return 10000
-
-def get_tfidf(term, document, corpus):
-
-    # print(get_tf(term, document))
-    # print(get_idf(term, corpus))
-
-    return get_tf(term, document) * get_idf(term, corpus)
-
-def get_keywords(sentence, corpus, numOfKeywords = 10):
-
+        token_list = []
+        for token in line.split():
+            token_list.append(token)
+        
+        tweet_list.append(line)
     
+normalised_tweets = [utils.canonize(tweet) for tweet in tweet_list]
+print(normalised_tweets[:5])
 
-    # Get the most frequent named entities (Using the raw string here for better entity recognition)
-    doc = nlp(sentence)
-    named_entities = GetNamedEntities(doc)
+normalised_tweets = [" ".join(tweet) for tweet in normalised_tweets]
 
-    entityCounts = {}
+print(normalised_tweets[:5])
 
-    for entity in named_entities:
-        if entity not in entityCounts:
-            entityCounts[entity] = sentence.count(entity)
+with open("normalised_tweets.txt", "w") as file:
+    doc = ""
+    for tweet in normalised_tweets:
+        doc += tweet
+        doc += "\n"
+    file.write(doc)
+'''
+normalised_tweets = []
 
+with open("normalised_tweets.txt", "r") as file:
+    for line in file.read().split("\n"):
+        normalised_tweets.append(line.split())
 
+print(normalised_tweets[:6])
 
+sentence = "Tried a new recipe for dinner"
 
-    # Get the most popular terms that are not entities
-    normalized_sentence = utils.canonize(RemoveNamedEntities(sentence, named_entities))
-    
-    mostRelevantWords = {}
+print(utils.get_keywords(sentence, normalised_tweets))
 
-
-    for term in normalized_sentence:
-        if term not in mostRelevantWords:
-
-            pos_tag = nltk.pos_tag(word_tokenize(term))[0][1]
-
-            mostRelevantWords[term] = [get_tfidf(term, normalized_sentence, corpus), pos_tag]
-    
-    return dict(sorted(mostRelevantWords.items(), key=lambda item: item[1], reverse=True)[:5]), dict(sorted(entityCounts.items(), key=lambda item: item[1], reverse=True)[:5])
+def catify(text_in):
