5302f34ede441825ebd75cec461749fb6d95e7e2
Shadow Kestrel <git-contact@mail.kestrel.gay>
2024-02-18 07:48:37 +0000

    (mostly working) deploy script

--- server/deploy.bash
+++ server/deploy.bash
@@ -1,2 +1,14 @@
+#!/usr/bin/bash
+
 cp ../index.html ../style.css ../script.js /var/www/html/catbot/
 echo 'http://kestrel.gay/catte.gif' > /var/www/html/catbot/next_gif.url
+
+# im not sure if python's socketserver will finish handling current requests
+# on SIGTERM but oh well
+kill -TERM $(ps -x | grep message_handler.py | awk '{print $1}')
+
+cp Python/message_handler.py run
+
+python run/message_handler.py 2>> run/catbot_ERR &
+
+
